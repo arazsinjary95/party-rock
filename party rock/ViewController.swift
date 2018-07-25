9cf9e3b087442e7b8cd53a649f2908bbf5b496a9
Araz Sinjary <araz@arazs-macbook-pro.local>
2018-07-25 20:12:25 +0300

    Initial Commit

---
+++ party rock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  party rock
+//
+//  Created by Araz Sinjary on 7/25/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
